b72f897090f013c8ae7c32ffc3c0e524c34d66fc
Karan Arora <arora15karan1996@gmail.com>
2017-01-27 15:03:41 +0530

    Update check_profanity.py

--- Programming_Foundations_With_Python/Lesson 2c(Use Classes)_Profanity_Editor/check_profanity.py
+++ Programming_Foundations_With_Python/Lesson 2c(Use Classes)_Profanity_Editor/check_profanity.py
@@ -3,7 +3,7 @@
 import urllib
 
 def read_file():
-    quotes = open("C:\Users\KARAN PC\Desktop\movie_quotes.txt");  #pah of the file
+    quotes = open("C:\Python27\Programming_Foundations_With_Python\Lesson 2c(Use Classes)_Profanity_Editor\movie_quotes.txt");  #pah of the file
     contents_of_file = quotes.read();
     print(contents_of_file)
     check_prof(contents_of_file)
