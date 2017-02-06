be1214f66a94011707a0d4e86519445a49fd3f91
Karan Arora <arora15karan1996@gmail.com>
2017-02-06 15:28:52 +0530

    Update media.py

--- Programming_Foundations_With_Python/Lesson 3a (Make Classes)_ Movie Website/movies/media.py
+++ Programming_Foundations_With_Python/Lesson 3a (Make Classes)_ Movie Website/movies/media.py
@@ -12,14 +12,16 @@ class Movie():
     """
    VALID_RATINGS = ["G", "PG","PG-13", "R"]
 
-   def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+   def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, rating, release_year):
       #Instance Variables
       self.title = movie_title       
       self.storyline = movie_storyline             
       self.poster_image_url = poster_image
       self.trailer_youtube_url = trailer_youtube
+      self.ratings = rating
+      self.year = release_year
 
       #Instace Methods
-   def show_trailer(self):
-      webbrowser.open(self.trailer_youtube_url)
+#   def show_trailer(self):
+#      webbrowser.open(self.trailer_youtube_url)
 # access the variables through access keyword
